884be9c5b44c59ed3e891f92993af99b09c1a1eb
Tim Won <tkw21189@gmail.com>
2015-08-21 21:54:00 -0500

    testing heroku

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -17,91 +17,6 @@ tv.filter('clean', function(){
 	};
 });
 
-tv.controller('SeasonsCtrl', function($scope, $http, $timeout, $interval, $rootScope){
-
-	//NYAN!!!
-	$scope.nyan = false;
-	$scope.changeNyan =  function(){
-		$scope.nyan = $scope.nyan === false ? true: false;
-
-	};
-	$scope.search = '';
-	$scope.json = {};
-	$scope.currentSeason = {};
-	var userSearch = {};
-
-	$scope.setSearch = function(search){
-		userSearch = search;
-	};
-
-
-
-	$scope.find_seasons = function(show){
-		$rootScope.show = show.replace(/\s/g, '');
-		$http({
-			method: 'get',
-			url: '/episodes/tv',
-			params: {search: show},
-			timeout: 3000
-		})
-			.success(function(data){
-				$scope.json = data;
-		});
-	};
-
-
-	$scope.setSeason = function(season){
-		$scope.currentSeason = $scope.json[season];
-	};
-
-
-	var evaluate = function(){
-		if (counter === 0) {
-			$interval.cancel(promise);
-			$scope.find_seasons($scope.search);
-			counter = 1;
-		} else {
-			tick();
-		}
-	};
-
-	var tick = function(){
-		counter -= 1;
-		console.log(counter);
-	};
-
-
-	var counter = 1;
-	var counting = false;
-	var promise;
-	
-
-	$scope.setTimer = function(){
-		$scope.stopTimer();
-		promise = $interval(function(){evaluate();}, 500);
-	};
-
-	$scope.stopTimer = function(){
-		$interval.cancel(promise);
-	};
-
-	$scope.timerAction = function(){
-		if (counter === 0) {
-			alert('hello there!');
-		} else {
-			counter = counter--;
-			console.log(counter);
-		}
-	};
-
-
-	$scope.timeout = function(){
-		$timeout(function(){
-			alert('hello');
-		}, 500);
-	};
-
-});
 
 tv.controller('TweetCtrl', function($scope,$http,$timeout, $rootScope){
 
@@ -138,7 +53,7 @@ tv.controller('TweetCtrl', function($scope,$http,$timeout, $rootScope){
 	$scope.setEpisode = function(airDate){
 
 		var date = airDate;
-		//Need to reorganize date into YYYY-MM-DD for the Twitter API
+		//Need to reor <i class="material-icons prefix">account_circle</i>ganize date into YYYY-MM-DD for the Twitter API
 		date = date.split('-');
 		date = {year: parseInt(date[0]), month: parseInt(date[1]), day: parseInt(date[2])};
 		//Setting +1 and +2 days after airdate to show relevent tweets
